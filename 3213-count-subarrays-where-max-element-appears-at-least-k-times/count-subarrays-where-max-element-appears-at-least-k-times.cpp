03cc81b6042cf962a3453f2c3ac2b38aecc04f5c
Abhinav Abhishek <121249325+AbhinavAbhishek77@users.noreply.github.com>
2024-03-29 11:32:56 +0530

    Time: 134 ms (55.43%) | Memory: 120.1 MB (62.40%) - LeetSync

--- 3213-count-subarrays-where-max-element-appears-at-least-k-times/count-subarrays-where-max-element-appears-at-least-k-times.cpp
+++ 3213-count-subarrays-where-max-element-appears-at-least-k-times/count-subarrays-where-max-element-appears-at-least-k-times.cpp
@@ -1,7 +1,34 @@
 class Solution {
 public:
+//TC==>O(n)
+//SC==>O(1)
     long long countSubarrays(vector<int>& nums, int k) {
 //APPROACH 1
+        // int n = nums.size();
+        // long long maxi = *max_element(nums.begin(), nums.end());
+        // long long cnt = 0, ans = 0;
+        // int i = 0, j = 0;
+        // while (j < n) {
+        //     if (nums[j] == maxi) {
+        //         cnt++;
+        //     }
+
+        //     if (cnt >= k) {
+        //         ans += (1 + n - 1 - j);//n-j
+        //         if (nums[i] == maxi) {
+        //             cnt--;
+        //         }
+        //         i++;
+        //         cnt--;
+        //         continue;
+        //     }
+        //     j++;
+        // }
+        // return ans;
+
+
+        //APPROACH 2
+
         int n = nums.size();
         long long maxi = *max_element(nums.begin(), nums.end());
         long long cnt = 0, ans = 0;
@@ -11,22 +38,17 @@ public:
                 cnt++;
             }
 
-            if (cnt >= k) {
-                ans += (1 + n - 1 - j);//n-j
+            while (cnt >= k) {
+                ans += (n-j);
                 if (nums[i] == maxi) {
                     cnt--;
                 }
                 i++;
-                cnt--;
-                continue;
             }
             j++;
         }
         return ans;
 
-        //APPROACH 2
-
-        
 
     }
 };
