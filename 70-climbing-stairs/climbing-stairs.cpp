12247f70c455211f7d7051825aa73a3b1cba53db
Abhinav Abhishek <121249325+AbhinavAbhishek77@users.noreply.github.com>
2024-06-03 12:58:13 +0530

    Time: 3 ms (29.34%) | Memory: 7.4 MB (11.43%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,22 +1,35 @@
 class Solution {
 public:
-    int solve(int index, vector<int>& dp) {
-        // Base cases
-        if (index == 0)
-            return 1;
-        if (index < 0)
-            return 0;
-        if (dp[index] != -1)
-            return dp[index];
-
-        // doing all the stuffs on the index acc. to problem
-        int left = solve(index - 1, dp);
-        int right = solve(index - 2, dp);
-
-        return dp[index] = left + right;
+    int tab(int index, vector<int>& dp) {
+        //bottom up: bae case to required answer
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i <= index; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[index];
     }
+    // int solve(int index, vector<int>& dp) {
+        //top down: required answer to base case
+    // TC:O(N)
+    // SC:O(N+N)
+    //     // Base cases
+    //     if (index == 0)
+    //         return 1;
+    //     if (index < 0)
+    //         return 0;
+    //     if (dp[index] != -1)
+    //         return dp[index];
+
+    //     // doing all the stuffs on the index acc. to problem
+    //     int left = solve(index - 1, dp);
+    //     int right = solve(index - 2, dp);
+
+    //     return dp[index] = left + right;
+    // }
     int climbStairs(int n) {
         vector<int> dp(n + 1, -1);
-        return solve(n, dp);
+        // return solve(n, dp);
+        return tab(n, dp);
     }
 };
