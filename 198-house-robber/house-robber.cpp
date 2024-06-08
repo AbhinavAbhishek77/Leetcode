7258ef430048259653a1a094b79631a5cd116270
Abhinav Abhishek <121249325+AbhinavAbhishek77@users.noreply.github.com>
2024-06-08 15:48:55 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (94.29%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,29 +1,52 @@
 class Solution {
 public:
-int tab(vector<int>&nums, int ind, vector<int>&dp)
+// int tab(vector<int>&nums, int ind, vector<int>&dp)
+// {
+//     //TC:O(N)
+//     //SC:O(N)
+//     dp[0]=nums[0];
+//     // if(ind<0) return 0;
+//     // if(dp[ind]!=-1) return dp[ind];
+
+//    for(int i=1;i<=ind;i++)
+//    {
+//        int pick=nums[i];
+//      if (i - 2 >= 0) {
+//        pick = nums[i] + dp[i - 2];
+//      }
+
+//        int notpick=0+dp[i-1];
+
+//        dp[i]=max(pick,notpick);
+//    }
+//    return dp[ind];
+// }
+
+
+int space(vector<int>&nums, int ind)
 {
-    //TC:O(N)
-    //SC:O(N)
-    dp[0]=nums[0];
-    // if(ind<0) return 0;
-    // if(dp[ind]!=-1) return dp[ind];
+    int prev=nums[0];; //dp[0]=nums[0];
+   int prev2=0;
 
    for(int i=1;i<=ind;i++)
    {
        int pick=nums[i];
      if (i - 2 >= 0) {
-       pick = nums[i] + dp[i - 2];
+       pick = nums[i] + prev2;
      }
 
-       int notpick=0+dp[i-1];
+       int notpick=0+prev;
 
-       dp[i]=max(pick,notpick);
+       int curr=max(pick,notpick);
+       prev2=prev;
+       prev=curr;
    }
-   return dp[ind];
+   return prev;
 }
     int rob(vector<int>& nums) {
         int n=nums.size();
-        vector<int>dp(n+1,-1);
-        return tab(nums,n-1,dp);
+        // vector<int>dp(n+1,-1);
+        // return tab(nums,n-1,dp);
+        return space(nums,n-1);
     }
 };
