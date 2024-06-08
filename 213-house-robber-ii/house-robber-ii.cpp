d35480afd7156e4eeda0fe71bdb1f01bf902ed00
Abhinav Abhishek <121249325+AbhinavAbhishek77@users.noreply.github.com>
2024-06-08 17:26:12 +0530

    Time: 4 ms (32.92%) | Memory: 9.5 MB (28.47%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,24 +1,33 @@
 class Solution {
 public:
-int mem(vector<int>&nums, int ind, vector<int>&dp)
-{
-    if(ind==0) return nums[0];
-    if(ind<0) return 0;
-    if(dp[ind]!=-1) return dp[ind];
+    // TC:O(N)
+    // SC:O(N)
+    int space(vector<int>& nums, int ind) {
+        int prev = nums[0];
+        int prev2 = 0;
 
-    int pick=nums[ind]+mem(nums,ind-2,dp);
-    int notpick=0+mem(nums,ind-1,dp);
+        for (int i = 1; i <= ind; i++) {
+            int pick = nums[i];
+            if (i - 2 >= 0) {
+                pick = nums[i] + prev2;
+            }
+            int notpick = 0 + prev;
 
-    return dp[ind]=max(pick,notpick);
-}
+            int curr = max(pick, notpick);
+            prev2 = prev;
+            prev = curr;
+        }
+        return prev;
+    }
     int rob(vector<int>& nums) {
-        int n=nums.size();
-        if(n==1) return nums[0];
-        vector<int>dp1(n+1,-1);
-        vector<int>dp2(n+1,-1);
+        int n = nums.size();
+        if (n == 1)
+            return nums[0];
 
-        vector<int>temp1(nums.begin(),nums.end()-1), temp2(nums.begin()+1,nums.end());
+        vector<int> temp1(nums.begin(), nums.end() - 1),
+            temp2(nums.begin() + 1, nums.end());
 
-        return max(mem(temp1,temp1.size()-1,dp1), mem(temp2,temp2.size()-1,dp2));
+        return max(space(temp1, temp1.size() - 1),
+                   space(temp2, temp2.size() - 1));
     }
 };
