aa57d8f1f721e985a79583a58ef6852e7373ee93
Abhinav Abhishek <121249325+AbhinavAbhishek77@users.noreply.github.com>
2024-06-12 17:51:07 +0530

    Time: 335 ms (15.75%) | Memory: 16.1 MB (67.47%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
-    bool mem(vector<int>& arr, int ind, int target, vector<vector<int>>& dp) {
-        if (ind == 0) {
-            if (target == 0)
-                return true;
-            else
-                return false;
-        }
-        if (dp[ind][target] != -1)
-            return dp[ind][target];
+    // bool mem(vector<int>& arr, int ind, int target, vector<vector<int>>& dp)
+    // {
+    //     if (ind == 0) {
+    //         if (target == 0)
+    //             return true;
+    //         else
+    //             return false;
+    //     }
+    //     if (dp[ind][target] != -1)
+    //         return dp[ind][target];
 
-        bool pick = false;
-        if (arr[ind] <= target) {
-            pick = mem(arr, ind - 1, target - arr[ind], dp);
-        }
-        bool notpick = mem(arr, ind - 1, target, dp);
+    //     bool pick = false;
+    //     if (arr[ind] <= target) {
+    //         pick = mem(arr, ind - 1, target - arr[ind], dp);
+    //     }
+    //     bool notpick = mem(arr, ind - 1, target, dp);
 
-        return dp[ind][target] = pick | notpick;
-    }
+    //     return dp[ind][target] = pick | notpick;
+    // }
 
     bool canPartition(vector<int>& nums) {
         int n = nums.size();
@@ -27,8 +28,34 @@ public:
         if (totalSum % 2 != 0)
             return false;
 
-        int target = totalSum / 2;
-        vector<vector<int>> dp(n + 1, vector<int>(target + 1, -1));
-        return mem(nums, n - 1, target, dp);
+        // int target = totalSum / 2;
+        // vector<vector<int>> dp(n + 1, vector<int>(target + 1, -1));
+        // return mem(nums, n - 1, target, dp);
+
+        int k = totalSum / 2;
+        vector<vector<bool>> dp(n + 1, vector<bool>(k + 1, false));
+        //    cout<<n<<endl;
+        for (int i = 0; i < n; i++) {
+            dp[i][0] = true;
+            //   cout<<i<<0<<"="<<dp[i][0]<<endl;
+        }
+
+        if (nums[0] <= k)
+            dp[0][nums[0]] = true;
+
+        for (int ind = 1; ind < n; ind++) {
+            for (int target = 1; target <= k; target++) {
+                bool notpick = dp[ind - 1][target];
+
+                bool pick = false;
+                if (nums[ind] <= target) {
+                    pick = dp[ind - 1][target - nums[ind]];
+                }
+
+                dp[ind][target] = pick | notpick;
+            }
+        }
+
+        return dp[n - 1][k];
     }
 };
