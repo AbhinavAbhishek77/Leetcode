840b638d938a9959ca1c1eaebb6d09caab1e701e
Abhinav Abhishek <121249325+AbhinavAbhishek77@users.noreply.github.com>
2024-06-22 00:55:52 +0530

    Time: 26 ms (48.29%) | Memory: 34.3 MB (47.97%) - LeetSync

--- 1138-grumpy-bookstore-owner/grumpy-bookstore-owner.cpp
+++ 1138-grumpy-bookstore-owner/grumpy-bookstore-owner.cpp
@@ -38,87 +38,50 @@ public:
     //     return total + extra;
     // }
     int maxSatisfied(vector<int>& customers, vector<int>& grumpy, int minutes) {
-        int n = customers.size();
+    int n = customers.size(); // Get the number of elements in the customers array
+    int maxi = INT_MIN; // Initialize the maximum value to the smallest possible integer
+    int ind = -1; // Initialize the index of the start of the optimal 'minutes' window
+    int un = 0; // Initialize the variable to store the number of unsatisfied customers
+    int i = 0; // Initialize a counter for the initial loop
 
-        // return ss(customers, grumpy, minutes);
-        int maxi = INT_MIN;
-        int ind = -1;
-        int un = 0;
-        int i = 0;
-        for (i = 0; i < minutes; i++) {
-            if (grumpy[i] == 1) {
-                un += customers[i];
-            }
+    // Calculate the number of unsatisfied customers in the first 'minutes' window
+    for (i = 0; i < minutes; i++) {
+        if (grumpy[i] == 1) {
+            un += customers[i]; // Add the number of customers if the owner is grumpy
         }
+    }
+    // Update maxi and ind if the current unsatisfied customers are more than maxi
+    if (un > maxi) {
+        maxi = un;
+        ind = i - minutes; // Set the start index of the optimal window
+    }
+
+    // Slide the window across the rest of the array
+    for (int j = i; j < n; j++) {
+        if (grumpy[j] == 1)
+            un += customers[j]; // Add customers if the owner is grumpy in the new position
+        if (grumpy[j - minutes] == 1)
+            un -= customers[j - minutes]; // Subtract customers if the owner was grumpy in the old position
+        // Update maxi and ind if the current unsatisfied customers are more than maxi
         if (un > maxi) {
             maxi = un;
-            ind = i - minutes;
+            ind = j - minutes + 1; // Set the start index of the optimal window
         }
-        for (int j = i; j < n; j++) {
-            if (grumpy[j] == 1)
-                un += customers[j];
-            if (grumpy[j - minutes] == 1)
-                un -= customers[j - minutes];
-            if (un > maxi) {
-                maxi = un;
-                ind = j - minutes + 1;
-            }
-        }
-
-        int sum = 0;
-        for (int i = 0; i < n; i++) {
-            if (i >= ind && i <= ind + minutes - 1) {
-                sum += customers[i];
-
-            }
-
-            else if (grumpy[i] == 0) {
-                sum += customers[i];
-            }
-        }
-
-        return sum;
-
-        // if (n == 1)
-        //     return customers[0];
-
-        // vector<int> ans(n);
-        // int maxi = INT_MIN;
-        // int ind = -1;
-
-        // for (int i = 0; i < n; i++) {
-        //     int add = 0;
-        //     for (int j = i; j < n && j < minutes + i; j++) {
-        //         if(grumpy[j]==1)
-        //         add += customers[j];
-        //     }
-        //     ans[i] = add;
-        //     if (grumpy[i]==1 && ans[i] > maxi) {
-        //         maxi = ans[i];
-        //         ind = i;
-        //     }
-        // }
-
-        // int maxsuminds = ind;
-        // int maxsuminde = ind + minutes - 1;
-
-        // int sum = 0;
-        // // maximum sum zone, where technique is used
-        // sum += ans[maxsuminds];
-
-        // // before maximum sum zone
-        // for (int i = 0; i < maxsuminds; i++) {
-        //     if (grumpy[i] != 1) {
-        //         sum += customers[i];
-        //     }
-        // }
-        // // after maximum sum zone
-        // for (int i = maxsuminde + 1; i < n; i++) {
-        //     if (grumpy[i] != 1) {
-        //         sum += customers[i];
-        //     }
-        // }
-
-        // return sum;
     }
+
+    int sum = 0; // Initialize the variable to store the total number of satisfied customers
+    for (int i = 0; i < n; i++) {
+        // If the current index is within the optimal window, add all customers
+        if (i >= ind && i <= ind + minutes - 1) {
+            sum += customers[i];
+        }
+        // Otherwise, add customers only if the owner is not grumpy
+        else if (grumpy[i] == 0) {
+            sum += customers[i];
+        }
+    }
+
+    return sum; // Return the total number of satisfied customers
+}
+
 };
