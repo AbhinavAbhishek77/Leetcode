9186b5d15c587da2dc6b4c5b4c59319a62b1931c
Abhinav Abhishek <121249325+AbhinavAbhishek77@users.noreply.github.com>
2024-06-22 19:08:25 +0530

    Time: 0 ms (100.00%) | Memory: 19.6 MB (60.58%) - LeetSync

---
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int solve(vector<int>& nums, int ind, int target, vector<vector<int>>& dp) {
+        // if (target == 0)
+        //     return 1;
+        if (ind == 0) {
+            return target % nums[0] == 0 ? 1 : 0;
+        }
+        if (dp[ind][target] != -1)
+            return dp[ind][target];
+        int notpick = 0 + solve(nums, ind - 1, target, dp);
+        int pick = 0;
+        if (nums[ind] <= target) {
+            pick = solve(nums, ind, target - nums[ind], dp);
+        }
+
+        return dp[ind][target] = notpick + pick;
+    }
+    int change(int amount, vector<int>& coins) {
+        int n = coins.size();
+        vector<vector<int>> dp(n, vector<int>(amount + 1, -1));
+        return solve(coins, n - 1, amount, dp);
+    }
+};
