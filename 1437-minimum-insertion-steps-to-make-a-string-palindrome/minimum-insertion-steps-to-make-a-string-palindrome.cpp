1dd841f11f8541f91fb02251b332757637c03799
Abhinav Abhishek <121249325+AbhinavAbhishek77@users.noreply.github.com>
2024-07-05 01:18:20 +0530

    Time: 36 ms (30.99%) | Memory: 29.7 MB (49.89%) - LeetSync

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
-    // Mem
-    int solve(int i, int j, string& ptr, string& str, vector<vector<int>>& dp) {
-        if (i == 0 || j == 0)
-            return 0;
-        if (dp[i][j] != -1)
-            return dp[i][j];
-
-        if (ptr[i - 1] == str[j - 1]) {
-            return dp[i][j] = 1 + solve(i - 1, j - 1, ptr, str, dp);
-        } else {
-            return dp[i][j] = max(solve(i, j - 1, ptr, str, dp),
-                                  solve(i - 1, j, ptr, str, dp));
-        }
-    }
-
+    // Tab
+    // dp[i][j] ==> till index [i][j] longest palindromic subsequence
     int minimumInsertions(string& str) {
         int n = str.size();
 
         string ptr = str;
         reverse(str.begin(), str.end());
-        vector<vector<int>> dp(n + 1, vector<int>(n + 1, -1));
-        return n - solve(n, n, ptr, str, dp);
+        vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
+
+        // Base condn already satisfied while declaring dp
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (ptr[i - 1] == str[j - 1]) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        return n - dp[n][n];
     }
     int minInsertions(string s) { return minimumInsertions(s); }
 };
