190dc50001ed6869788cd427b22eab0d30d96f0a
Abhinav Abhishek <121249325+AbhinavAbhishek77@users.noreply.github.com>
2024-07-11 19:55:09 +0530

    Time: 15 ms (74.45%) | Memory: 20.1 MB (49.57%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,28 +1,35 @@
 class Solution {
 public:
-    // 1-based indexing
+    // Tabulation
+    //  1-based indexing
     int mod = 1e9 + 7;
-    int solve(string& str, string& sub, int i, int j, vector<vector<int>>& dp) {
-        if (j == 0)
-            return 1;
-        if (i == 0)
-            return 0;
-        if (dp[i][j] != -1)
-            return dp[i][j] % mod;
-
-        if (str[i - 1] == sub[j - 1]) {
-            return dp[i][j] = (solve(str, sub, i - 1, j - 1, dp) % mod +
-                               solve(str, sub, i - 1, j, dp) % mod) %
-                              mod;
-        } else {
-            return dp[i][j] = solve(str, sub, i - 1, j, dp) % mod;
-        }
-    }
+    // dp[i][j] means Count of all distinct subsequences of S2[0...j-1]
+    // in the string S1[0...i-1].
     int distinctSubsequences(string& str, string& sub) {
         int n = str.size();
         int m = sub.size();
-        vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1));
-        return solve(str, sub, n, m, dp) % mod;
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = 1; // An empty sub is a subsequence of any string
+        }
+        for (int j = 1; j <= m; j++) {
+            dp[0][j] =
+                0; // Non-empty sub cannot be a subsequence of an empty string
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (str[i - 1] == sub[j - 1]) {
+                    dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j]) % mod;
+                } else {
+                    dp[i][j] = dp[i - 1][j] % mod;
+                }
+            }
+        }
+
+        return dp[n][m];
     }
+
     int numDistinct(string s, string t) { return distinctSubsequences(s, t); }
 };
