a1f3e1c2ab35f0d8acbfc7714ba75d6058160c23
Abhinav Abhishek <121249325+AbhinavAbhishek77@users.noreply.github.com>
2024-07-11 19:54:13 +0530

    Time: 19 ms (57.79%) | Memory: 20.2 MB (49.57%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
+    // 1-based indexing
     int mod = 1e9 + 7;
     int solve(string& str, string& sub, int i, int j, vector<vector<int>>& dp) {
-        if (j < 0)
+        if (j == 0)
             return 1;
-        if (i < 0)
+        if (i == 0)
             return 0;
         if (dp[i][j] != -1)
             return dp[i][j] % mod;
 
-        if (str[i] == sub[j]) {
+        if (str[i - 1] == sub[j - 1]) {
             return dp[i][j] = (solve(str, sub, i - 1, j - 1, dp) % mod +
                                solve(str, sub, i - 1, j, dp) % mod) %
                               mod;
@@ -17,12 +18,11 @@ public:
             return dp[i][j] = solve(str, sub, i - 1, j, dp) % mod;
         }
     }
-
     int distinctSubsequences(string& str, string& sub) {
         int n = str.size();
         int m = sub.size();
-        vector<vector<int>> dp(n, vector<int>(m, -1));
-        return solve(str, sub, n - 1, m - 1, dp) % mod;
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1));
+        return solve(str, sub, n, m, dp) % mod;
     }
     int numDistinct(string s, string t) { return distinctSubsequences(s, t); }
 };
