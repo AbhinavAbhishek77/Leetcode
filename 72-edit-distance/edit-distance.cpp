bf1fbf9f4e5168fb6d2b3e295bc8033e960470e7
Abhinav Abhishek <121249325+AbhinavAbhishek77@users.noreply.github.com>
2024-07-13 18:33:58 +0530

    Time: 13 ms (29.71%) | Memory: 13 MB (18.52%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,25 +1,5 @@
 class Solution {
 public:
-    int solve(string& s, string& t, int i, int j, vector<vector<int>>& dp) {
-
-        if (i == 0)
-            return j;
-        if (j == 0)
-            return i;
-        if (dp[i][j] != -1)
-            return dp[i][j];
-        if (s[i - 1] == t[j - 1]) {
-            return dp[i][j] = solve(s, t, i - 1, j - 1, dp);
-        } else {
-            // insert
-            // delete
-            // replace
-            return dp[i][j] = min(min(1 + solve(s, t, i, j - 1, dp),
-                                      1 + solve(s, t, i - 1, j, dp)),
-                                  1 + solve(s, t, i - 1, j - 1, dp));
-        }
-    }
-
     int tabulation(string& s, string& t) {
         int n = s.size(), m = t.size();
         vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
@@ -36,7 +16,7 @@ public:
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= m; j++) {
                 if (s[i - 1] == t[j - 1]) {
-                    dp[i][j] = solve(s, t, i - 1, j - 1, dp);
+                    dp[i][j] = dp[i - 1][j - 1];
                 } else {
                     // insert
                     // delete
@@ -50,9 +30,6 @@ public:
         return dp[n][m];
     }
     int minDistance(string word1, string word2) {
-        int n = word1.size();
-        int m = word2.size();
-        vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1));
-        return solve(word1, word2, n, m, dp);
+        return tabulation(word1, word2);
     }
 };
