6bfc2186e90b07a163ef321fdfc3031c4d2c2f25
Abhinav Abhishek <121249325+AbhinavAbhishek77@users.noreply.github.com>
2024-07-13 18:38:40 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (81.18%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -2,32 +2,35 @@ class Solution {
 public:
     int tabulation(string& s, string& t) {
         int n = s.size(), m = t.size();
-        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+        // vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+        vector<int> prev(m + 1, 0), curr(m + 1, 0);
 
         // Base Case
-        for (int i = 0; i <= n; i++) {
-            dp[i][0] = i;
-        }
+        // for (int i = 0; i <= n; i++) {
+        //     curr[0] = i;
+        // }
 
         for (int j = 0; j <= m; j++) {
-            dp[0][j] = j;
+            prev[j] = j;
         }
 
         for (int i = 1; i <= n; i++) {
+            curr[0]=i;
             for (int j = 1; j <= m; j++) {
                 if (s[i - 1] == t[j - 1]) {
-                    dp[i][j] = dp[i - 1][j - 1];
+                    curr[j] = prev[j - 1];
                 } else {
                     // insert
                     // delete
                     // replace
-                    dp[i][j] = min(min(1 + dp[i][j - 1], 1 + dp[i - 1][j]),
-                                   1 + dp[i - 1][j - 1]);
+                    curr[j] =
+                        min(min(1 + curr[j - 1], 1 + prev[j]), 1 + prev[j - 1]);
                 }
             }
+            prev = curr;
         }
 
-        return dp[n][m];
+        return prev[m];
     }
     int minDistance(string word1, string word2) {
         return tabulation(word1, word2);
