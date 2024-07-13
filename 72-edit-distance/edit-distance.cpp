aebfe0c4d1fadc8a729925ec3ebea2c50598da60
Abhinav Abhishek <121249325+AbhinavAbhishek77@users.noreply.github.com>
2024-07-13 10:13:08 +0530

    Time: 11 ms (35.23%) | Memory: 12.9 MB (18.52%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -19,6 +19,36 @@ public:
                                   1 + solve(s, t, i - 1, j - 1, dp));
         }
     }
+
+    int tabulation(string& s, string& t) {
+        int n = s.size(), m = t.size();
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+
+        // Base Case
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = i;
+        }
+
+        for (int j = 0; j <= m; j++) {
+            dp[0][j] = j;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (s[i - 1] == t[j - 1]) {
+                    dp[i][j] = solve(s, t, i - 1, j - 1, dp);
+                } else {
+                    // insert
+                    // delete
+                    // replace
+                    dp[i][j] = min(min(1 + dp[i][j - 1], 1 + dp[i - 1][j]),
+                                   1 + dp[i - 1][j - 1]);
+                }
+            }
+        }
+
+        return dp[n][m];
+    }
     int minDistance(string word1, string word2) {
         int n = word1.size();
         int m = word2.size();
