f4958ad7c0b1122c6b6144c718e9e8da39c8b200
Abhinav Abhishek <121249325+AbhinavAbhishek77@users.noreply.github.com>
2024-08-02 23:02:58 +0530

    Time: 146 ms (32.43%) | Memory: 73.3 MB (27.14%) - LeetSync

---
+++ 1325-path-with-maximum-probability/path-with-maximum-probability.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    double maxProbability(int n, vector<vector<int>>& edges,
+                          vector<double>& succProb, int start_node,
+                          int end_node) {
+
+        // make adjacency list
+
+        vector<pair<int, double>> adj[n];
+        int i = 0;
+        for (auto it : edges) {
+            int u = it[0];
+            int v = it[1];
+            double wt = succProb[i++];
+            adj[u].push_back({v, wt});
+            adj[v].push_back({u, wt});
+        }
+        // for (int i = 0; i < n; i++) {
+        //     for (auto v : adj[i])
+        //         cout << v.first << " " << v.second << endl;
+        // }
+
+        priority_queue<pair<double, int>> pq;
+        // priority_queue<pair<int, int>, vector<pair<int, int>>,
+        //                greater<pair<int, int>>>
+        //     pq;
+        // queue<pair<double, int>> pq;
+
+        vector<double> dist(n, 0);
+        dist[start_node] = 1;
+        pq.push({1, start_node});
+
+        while (!pq.empty()) {
+            int node = pq.top().second;
+            double w = pq.top().first;
+            pq.pop();
+
+            for (auto it : adj[node]) {
+                int nextNode = it.first;
+                double weight = it.second;
+
+                if (dist[node] * weight > dist[nextNode]) {
+                    dist[nextNode] = dist[node] * weight;
+                    pq.push({dist[nextNode], nextNode});
+                }
+            }
+        }
+        // for (auto v : dist) {
+        //     cout << v << endl;
+        // }
+        return dist[end_node];
+    }
+};
